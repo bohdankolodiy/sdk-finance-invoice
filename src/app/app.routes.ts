6b597ef45f79cc92d4a4021d939442d784f770ee
bkolodiy <bkolodiy@binariks.com>
2024-06-21 15:54:13 +0300

    Create front-office page & sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'front-office',
+    loadChildren: () =>
+      import('./front-office/front-office.routes').then((m) => m.routes),
+  },
+  {
+    path: '**',
+    redirectTo: 'front-office',
+  },
+];
